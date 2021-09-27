fbcfdccaf65fe55d3755e756cf37cbbbebcb95ee
kulkarnia21 <38009428+kulkarnia21@users.noreply.github.com>
2021-09-26 22:05:49 -0400

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -10,18 +10,19 @@ def perceptron(input_array, T):
         for i in range(len(input_array)):
             print('i = ' + str(i) + '| t = ' + str(t))
             print(theta)
-            #print(input_array[i])
-            # Separate input array into x_i (inputs) and y_i (expected output)
+            # print(input_array[i])
+            
+            ##### Separate input array into x_i (inputs) and y_i (expected output)
             x_i = input_array[i][:-1]
-            #print(x_i)
+            # print(x_i)
             y_i = input_array[i][-1]
-            #print(y_i)
-            # check if guess is correct, update if incorrect
+            # print(y_i)
+            ##### check if guess is correct, update if incorrect
             if y_i * np.dot(theta, x_i) <= 0:
                 theta = theta + y_i * x_i
                 print('updated on i = ' + str(i), ' t = ' + str(t))
 
-
+                
 perceptron(tst, 2)
 
 print('initiating with x_2----------------------')
