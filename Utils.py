4c85e8215dd1f1fbb4bad2a2d63fa7b254ba65e2
kulkarnia21 <38009428+kulkarnia21@users.noreply.github.com>
2021-09-26 21:53:42 -0400

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -5,7 +5,7 @@ tst = np.array([[-1, -1, 1], [1, 0, -1], [-1, 1.5, 1]])
 def perceptron(input_array, T):
     ## inputs
     # T = number of epochs
-    theta = np.zeros((tst.shape[1] - 1))
+    theta = np.zeros((input_array.shape[1] - 1))
     for t in range(T):
         for i in range(len(input_array)):
             print('i = ' + str(i) + '| t = ' + str(t))
